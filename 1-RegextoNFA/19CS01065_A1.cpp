4856b4b29dbf81b6df0d35543337d5d769ea45f7
Shrirang Deshmukh <deshmukhshrirang01@gmail.com>
2022-10-31 17:32:04 +0530

    Display FSM in MD

--- 1-RegextoNFA/19CS01065_A1.cpp
+++ 1-RegextoNFA/19CS01065_A1.cpp
@@ -12,90 +12,116 @@ using namespace std;
 
 // ID to uniquely identify each node created in the process
 int currentNodeCount = 0;
+FILE *fp;
 
 // Class representing node in the FSM
-class Node {
-   public:
+class Node
+{
+public:
     int data;
     vector<pair<Node *, char>> adjList;
-    Node() {
+    Node()
+    {
         this->data = currentNodeCount++;
         vector<pair<Node *, char>> vec;
         this->adjList = vec;
     }
-    void addTransition(Node *dst, char character) {
-        if (dst == NULL) return;
+    void addTransition(Node *dst, char character)
+    {
+        if (dst == NULL)
+            return;
         adjList.push_back(make_pair(dst, character));
     }
 
-    void printEdges() {
-        if (adjList.size() > 0) {
-            for (auto const &adj : adjList) {
+    void printEdges()
+    {
+        if (adjList.size() > 0)
+        {
+            for (auto const &adj : adjList)
+            {
                 printf("%c -> %d |\t ", adj.second, adj.first->data);
+                fprintf(fp, "%d ==%c==> %d\n", data, adj.second, adj.first->data);
             }
         }
     }
 };
 
 // FSM to perform the operations
-class FSM {
-   public:
+class FSM
+{
+public:
     Node *start;
     Node *end;
     vector<Node *> nodes;
 
-    FSM() {
+    FSM()
+    {
         this->start = NULL;
         this->end = NULL;
         this->nodes = vector<Node *>();
     }
 
-    FSM(char character) {
+    FSM(char character)
+    {
         this->start = new Node();
         this->nodes = vector<Node *>{start};
 
-        if (isalpha(character)) {
+        if (isalpha(character))
+        {
             this->end = new Node();
             nodes.push_back(end);
             start->addTransition(end, character);
-        } else if (character == EPSILON_SYMBOL) {
+        }
+        else if (character == EPSILON_SYMBOL)
+        {
             this->end = start;
-        } else {
+        }
+        else
+        {
             this->end = NULL;
             nodes.push_back(end);
         }
     }
 
-    void displayFSM() {
+    void displayFSM()
+    {
         printf("\n\tTransition Table \n");
         printf("_____________________________________\n");
         printf("Current State -\tInput -> Next State");
         printf("\n_____________________________________\n");
 
         printf("S %d |\t", start->data);
+        fprintf(fp, "%d\n", start->data);
         start->printEdges();
         printf("\n");
 
-        for (auto const &node : nodes) {
-            if (node != start && node != end) {
+        for (auto const &node : nodes)
+        {
+            if (node != start && node != end)
+            {
                 printf("I %d |\t", node->data);
                 node->printEdges();
                 printf("\n");
             }
         }
-        if (end == NULL) {
+        if (end == NULL)
+        {
             printf("No end node |\t\n");
-        } else {
+        }
+        else
+        {
             printf("E %d |\t\n", end->data);
         }
     };
 
-    void union_operation(FSM next) {
+    void union_operation(FSM next)
+    {
         // PHI machine is uniquely identified by its end being NULL
         // EPSILON machine is identified by its start = end
 
         // addition with phi		=> a + % = a
-        if (end == NULL) {
+        if (end == NULL)
+        {
             // current FSM is PHI
             // phi + next FSM is nextFSM
             start = next.start;
@@ -105,10 +131,14 @@ class FSM {
             // all elements from next FSM
             nodes.pop_back();
             nodes.insert(nodes.end(), next.nodes.begin(), next.nodes.end());
-        } else if (next.end == NULL) {
+        }
+        else if (next.end == NULL)
+        {
             // nextFSM is PHI
             // no need to change anything keep it as it is
-        } else {
+        }
+        else
+        {
             Node *newStart = new Node();
             Node *newEnd = new Node();
 
@@ -128,39 +158,52 @@ class FSM {
         }
     };
 
-    void concat_operation(FSM next) {
+    void concat_operation(FSM next)
+    {
         // multiplication with phi		=> a.% = %
         // multiplication with epsilon	=> a.$ = a
-        if (start == NULL && end == NULL) {
+        if (start == NULL && end == NULL)
+        {
             start = next.start;
             nodes.insert(nodes.end(), next.nodes.begin(), next.nodes.end());
             end = next.end;
-        } else if (start != NULL && end == NULL) {
+        }
+        else if (start != NULL && end == NULL)
+        {
             // phi.something = phi
             // no no need to anything
-        } else if (next.start != NULL && next.end == NULL) {
+        }
+        else if (next.start != NULL && next.end == NULL)
+        {
             // something non_phi.phi = phi
             // make it phi
 
             start = next.start;
             nodes = next.nodes;
             end = next.end;
-        } else if (next.start != NULL && next.end == next.start) {
+        }
+        else if (next.start != NULL && next.end == next.start)
+        {
             // something.epsilon = something
             // no need to anything
-        } else if (start != NULL && start == end) {
+        }
+        else if (start != NULL && start == end)
+        {
             // epsilon*something = something
             start = next.start;
             nodes = next.nodes;
             end = next.end;
-        } else {
+        }
+        else
+        {
             end->addTransition(next.start, '$');
             nodes.insert(nodes.end(), next.nodes.begin(), next.nodes.end());
             end = next.end;
         }
     };
 
-    void kleene_closure_operation() {
+    void kleene_closure_operation()
+    {
         Node *newStart = new Node();
         Node *newEnd = new Node();
 
@@ -179,21 +222,30 @@ class FSM {
 };
 
 // class for validating regex
-class RegexValidator {
-   private:
+class RegexValidator
+{
+private:
     string expr;
 
-    bool areParenthesesBalanced() {
+    bool areParenthesesBalanced()
+    {
         stack<char> st;
         int l = expr.length();
 
-        for (auto ch : expr) {
-            if (ch == LEFT_PARENTHESIS) {
+        for (auto ch : expr)
+        {
+            if (ch == LEFT_PARENTHESIS)
+            {
                 st.push(ch);
-            } else if (ch == RIGHT_PARENTHESIS) {
-                if (!st.empty() && st.top() == LEFT_PARENTHESIS) {
+            }
+            else if (ch == RIGHT_PARENTHESIS)
+            {
+                if (!st.empty() && st.top() == LEFT_PARENTHESIS)
+                {
                     st.pop();
-                } else {
+                }
+                else
+                {
                     return false;
                 }
             }
@@ -201,55 +253,68 @@ class RegexValidator {
         return st.empty();
     }
 
-    bool validCharacterCheck(char ch) {
+    bool validCharacterCheck(char ch)
+    {
         return isalpha(ch) || ch == LEFT_PARENTHESIS ||
                ch == RIGHT_PARENTHESIS || ch == UNION_OPERATOR ||
                ch == CONCATENATION_OPERATOR || ch == KLEENE_CLOSURE_OPERATOR ||
                ch == EPSILON_SYMBOL || ch == PHI_SYMBOL;
     }
 
-    bool operatorsCheck() {
+    bool operatorsCheck()
+    {
         // + must be followed by (, alpha, % or $
         // . must be followed by (, alpha, % or $
         // ( must be followed by (, alpha, % or $
 
-        /* 
-			* must be followed by ., +,) 
-			) must be followed by ., +,), or *
-			alpha must be followed by ., +,), or *,
-			$ must be followed by ., +,), or *,
-			% must be followed by ., +,), or *,
-		*/
+        /*
+            * must be followed by ., +,)
+            ) must be followed by ., +,), or *
+            alpha must be followed by ., +,), or *,
+            $ must be followed by ., +,), or *,
+            % must be followed by ., +,), or *,
+        */
 
         int l = expr.length();
         int i = 0;
-        while (i < l) {
+        while (i < l)
+        {
             char ch = expr[i];
 
-            if (i == 0) {
+            if (i == 0)
+            {
                 if (!(isalpha(ch) || ch == LEFT_PARENTHESIS ||
-                      ch == EPSILON_SYMBOL || ch == PHI_SYMBOL)) {
+                      ch == EPSILON_SYMBOL || ch == PHI_SYMBOL))
+                {
                     return false;
                 }
             }
 
             if (ch == UNION_OPERATOR || ch == CONCATENATION_OPERATOR ||
-                ch == LEFT_PARENTHESIS) {
-                if (i + 1 > l) return false;
+                ch == LEFT_PARENTHESIS)
+            {
+                if (i + 1 > l)
+                    return false;
                 char nextChar = expr[i + 1];
                 if (!(nextChar == LEFT_PARENTHESIS || nextChar == PHI_SYMBOL ||
                       nextChar == EPSILON_SYMBOL || isalpha(nextChar)))
                     return false;
-            } else if (ch == KLEENE_CLOSURE_OPERATOR) {
-                if (i + 1 < l) {
+            }
+            else if (ch == KLEENE_CLOSURE_OPERATOR)
+            {
+                if (i + 1 < l)
+                {
                     char nextChar = expr[i + 1];
                     if (!(nextChar == UNION_OPERATOR ||
                           nextChar == CONCATENATION_OPERATOR ||
                           nextChar == RIGHT_PARENTHESIS))
                         return false;
                 }
-            } else {
-                if (i + 1 < l) {
+            }
+            else
+            {
+                if (i + 1 < l)
+                {
                     char nextChar = expr[i + 1];
                     if (!(nextChar == UNION_OPERATOR ||
                           nextChar == CONCATENATION_OPERATOR ||
@@ -264,23 +329,28 @@ class RegexValidator {
         return true;
     }
 
-   public:
+public:
     RegexValidator(string expression) { this->expr = expression; }
 
-    bool isRegexValid() {
-        for (auto ch : expr) {
-            if (!validCharacterCheck(ch)) {
+    bool isRegexValid()
+    {
+        for (auto ch : expr)
+        {
+            if (!validCharacterCheck(ch))
+            {
                 cout << "Invalid character " << ch << "in " << expr << endl;
                 return false;
             }
         }
 
-        if (!areParenthesesBalanced()) {
+        if (!areParenthesesBalanced())
+        {
             cout << "Unbalanced parentheses in " << expr << endl;
             return false;
         }
 
-        if (!operatorsCheck()) {
+        if (!operatorsCheck())
+        {
             cout << "Invalid operators in " << expr << endl;
             return false;
         }
@@ -288,15 +358,18 @@ class RegexValidator {
         return true;
     }
 
-    string preProcessRegex() {
+    string preProcessRegex()
+    {
         string result = "";
         int l = expr.length();
         int i = 0;
-        while (i < l) {
+        while (i < l)
+        {
             // eliminate %*and $*and replace them with % and % resp;
             result.push_back(expr[i]);
             if ((expr[i] == EPSILON_SYMBOL || expr[i] == PHI_SYMBOL) &&
-                i + 1 < l && expr[i + 1] == KLEENE_CLOSURE_OPERATOR) {
+                i + 1 < l && expr[i + 1] == KLEENE_CLOSURE_OPERATOR)
+            {
                 i++;
             }
             i++;
@@ -306,69 +379,92 @@ class RegexValidator {
     }
 };
 
-FSM analyzeRegex(string expr) {
+FSM analyzeRegex(string expr)
+{
     cout << "Entering analyzeRegex with " << expr << endl;
     FSM final = FSM();
     int l = expr.length();
     int i = 0;
-    while (i < l) {
+    while (i < l)
+    {
         char current = expr[i];
         cout << current << " " << endl;
-        if (current == LEFT_PARENTHESIS) {
+        if (current == LEFT_PARENTHESIS)
+        {
             int k = i;
             int j = 0;
-            while (expr[i++] != RIGHT_PARENTHESIS) j++;
+            while (expr[i++] != RIGHT_PARENTHESIS)
+                j++;
 
             string sub = expr.substr(k + 1, j - 1);
             FSM internal = analyzeRegex(sub);
-            if (i < l && expr[i] == '*') {
+            if (i < l && expr[i] == '*')
+            {
                 internal.kleene_closure_operation();
                 i++;
             }
             final.concat_operation(internal);
-        } else if (current == KLEENE_CLOSURE_OPERATOR) {
+        }
+        else if (current == KLEENE_CLOSURE_OPERATOR)
+        {
             final.kleene_closure_operation();
             i++;
-        } else if (current == CONCATENATION_OPERATOR) {
+        }
+        else if (current == CONCATENATION_OPERATOR)
+        {
             char nextChar = expr[i + 1];
-            if (nextChar == LEFT_PARENTHESIS) {
+            if (nextChar == LEFT_PARENTHESIS)
+            {
                 int k = i;
                 int j = 0;
-                while (expr[i++] != RIGHT_PARENTHESIS) j++;
+                while (expr[i++] != RIGHT_PARENTHESIS)
+                    j++;
 
                 string sub = expr.substr(k + 1, j - 1);
                 FSM internal = analyzeRegex(sub);
-                if (i < l && expr[i] == '*') {
+                if (i < l && expr[i] == '*')
+                {
                     internal.kleene_closure_operation();
                     i++;
                 }
                 final.concat_operation(internal);
-            } else {
+            }
+            else
+            {
                 FSM temp = FSM(nextChar);
                 i++;
-                if (i + 1 < l && expr[i + 1] == '*') {
+                if (i + 1 < l && expr[i + 1] == '*')
+                {
                     temp.kleene_closure_operation();
                     i++;
                 }
                 final.concat_operation(temp);
             }
             i++;
-        } else if (current == UNION_OPERATOR) {
+        }
+        else if (current == UNION_OPERATOR)
+        {
             string sub = expr.substr(i + 1, l - i + 1);
             FSM internal = analyzeRegex(sub);
             final.union_operation(internal);
             i = l;
-        } else if (isalpha(current) || current == EPSILON_SYMBOL ||
-                   current == PHI_SYMBOL) {
+        }
+        else if (isalpha(current) || current == EPSILON_SYMBOL ||
+                 current == PHI_SYMBOL)
+        {
             FSM temp = FSM(current);
-            if (i + 1 < l && expr[i + 1] == '*') {
+            if (i + 1 < l && expr[i + 1] == '*')
+            {
                 temp.kleene_closure_operation();
                 i++;
             }
 
-            if (final.start == NULL) {
+            if (final.start == NULL)
+            {
                 final = temp;
-            } else {
+            }
+            else
+            {
                 final.concat_operation(temp);
             }
             i++;
@@ -377,7 +473,8 @@ FSM analyzeRegex(string expr) {
     return final;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     cout << "Program to convert Regex to NFA" << endl;
     string expression;
     cin >> expression;
@@ -385,7 +482,8 @@ int main(int argc, char **argv) {
 
     bool isValid = regexValidator.isRegexValid();
     cout << (isValid ? "Valid Regex" : "Invalid Regex") << endl;
-    if (!isValid) {
+    if (!isValid)
+    {
         return -1;
     }
 
@@ -394,7 +492,10 @@ int main(int argc, char **argv) {
     system("cls");
     cout << "Expression: " << expression << endl;
     cout << "Processed Expression: " << expr << endl;
+    fp = fopen("Output.md", "w");
+    fprintf(fp, "```mermaid\ngraph LR\n");
     finalFSM.displayFSM();
+    fprintf(fp, "```\n");
 
     return 0;
 }
